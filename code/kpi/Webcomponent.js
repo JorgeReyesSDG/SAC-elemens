2af8e94e58e2389a3c66e687f27d3bf3899f3f78
JorgeReyesSDG <jorge.reyes@sdggroup.com>
2023-09-22 12:28:07 +0200

    sapui inherit

--- code/kpi/Webcomponent.js
+++ code/kpi/Webcomponent.js
@@ -10,9 +10,23 @@
         }
 
         init() {            
-              
+            // shadow dom
             let shadowRoot = this.attachShadow({mode: "open"});
             shadowRoot.appendChild(tmpl.content.cloneNode(true));
+
+            // inherit sap object
+                // Create the ComparisonMicroChart
+            const comparisonChart = new sap.suite.ui.microchart.ComparisonMicroChart({
+                id: "comparisonChart", // Assign an ID to the chart
+                    // Set other chart properties and data here
+                });
+
+                // Attach the chart to the HTML element
+            const chartContainer = shadowRoot.getElementById("comparisonChart");
+            comparisonChart.placeAt(chartContainer);
+
+
+            // functionallity
             this.addEventListener("click", event => {
             var event = new Event("onClick");
             this.fireChanged();           
