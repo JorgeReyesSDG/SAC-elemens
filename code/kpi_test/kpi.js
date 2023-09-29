a9a9dffb065e85345c1ee80c6e6e5195547c9c14
JorgeReyesSDG <jorge.reyes@sdggroup.com>
2023-09-29 12:05:06 +0200

    supressing not needed details

--- code/kpi_test/kpi.js
+++ code/kpi_test/kpi.js
@@ -51,101 +51,7 @@
             this.addEventListener("click", event => {
                 console.log('click');
             });
-        }
-
-        connectedCallback() {
-            try {
-                if (window.commonApp) {
-                    let outlineContainer = commonApp.getShell().findElements(true, ele => ele.hasStyleClass && ele.hasStyleClass("sapAppBuildingOutline"))[0]; // sId: "__container0"
-
-                    if (outlineContainer && outlineContainer.getReactProps) {
-                        let parseReactState = state => {
-                            let components = {};
-
-                            let globalState = state.globalState;
-                            let instances = globalState.instances;
-                            let app = instances.app["[{\"app\":\"MAIN_APPLICATION\"}]"];
-                            let names = app.names;
-
-                            for (let key in names) {
-                                let name = names[key];
-
-                                let obj = JSON.parse(key).pop();
-                                let type = Object.keys(obj)[0];
-                                let id = obj[type];
-
-                                components[id] = {
-                                    type: type,
-                                    name: name
-                                };
-                            }
-
-                            for (let componentId in components) {
-                                let component = components[componentId];
-                            }
-
-                            let metadata = JSON.stringify({
-                                components: components,
-                                vars: app.globalVars
-                            });
-
-                            if (metadata != this.metadata) {
-                                this.metadata = metadata;
-
-                                this.dispatchEvent(new CustomEvent("propertiesChanged", {
-                                    detail: {
-                                        properties: {
-                                            metadata: metadata
-                                        }
-                                    }
-                                }));
-                            }
-                        };
-
-                        let subscribeReactStore = store => {
-                            this._subscription = store.subscribe({
-                                effect: state => {
-                                    parseReactState(state);
-                                    return {
-                                        result: 1
-                                    };
-                                }
-                            });
-                        };
-
-                        let props = outlineContainer.getReactProps();
-                        if (props) {
-                            subscribeReactStore(props.store);
-                        } else {
-                            let oldRenderReactComponent = outlineContainer.renderReactComponent;
-                            outlineContainer.renderReactComponent = e => {
-                                let props = outlineContainer.getReactProps();
-                                subscribeReactStore(props.store);
-
-                                oldRenderReactComponent.call(outlineContainer, e);
-                            }
-                        }
-                    }
-                }
-            } catch (e) {}
-        }
-
-        disconnectedCallback() {
-            if (this._subscription) { 
-                this._subscription();
-                this._subscription = null;
-            }
-        }
-
-        onCustomWidgetBeforeUpdate(changedProperties) {
-            if ("designMode" in changedProperties) {
-                this._designMode = changedProperties["designMode"];
-            }
-        }
-
-        onCustomWidgetAfterUpdate(changedProperties) {
-            loadthis(this);
-        }
+        }  
 
         _firePropertiesChanged() {
             this.password = "";
@@ -158,29 +64,8 @@
             }));
         }
 
-        // SETTINGS
-        get password() {
-            return this._export_settings.password;
-        }
-        set password(value) {
-            value = _password;
-            this._export_settings.password = value;
-        }
-
-        static get observedAttributes() {
-            return [
-                "password"
-            ];
-        }
-
-        attributeChangedCallback(name, oldValue, newValue) {
-            if (oldValue != newValue) {
-                this[name] = newValue;
-            }
-        }
-
     }
-    customElements.define("com-fd-djaja-sap-sac-inputpassword", InputPassword);
+    customElements.define("jorge-custom-sap-kpi_test", InputPassword);
 
     // UTILS
     function loadthis(that) {
