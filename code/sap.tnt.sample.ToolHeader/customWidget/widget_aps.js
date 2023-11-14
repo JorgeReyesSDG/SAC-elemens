24426bdd6f1415a42e0cd97a0fe3e3d80ced5282
JorgeReyesSDG <jorge.reyes@sdggroup.com>
2023-11-14 15:22:50 +0100

    refresh the version

--- code/sap.tnt.sample.ToolHeader/customWidget/widget_aps.js
+++ code/sap.tnt.sample.ToolHeader/customWidget/widget_aps.js
@@ -1,7 +1,7 @@
 (function () {
     let tmpl = document.createElement("template");
     tmpl.innerHTML = 
-    `<button type="button" id="myHeaderBtn">Hi Button</button>` ;   
+    `<button type="button" id="myHeaderBtn">v 21</button>` ;   
 
     class CustomHeaderAps extends HTMLElement {
        constructor() {
