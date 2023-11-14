b64e95483c068be030f048b36427cda418932fdd
JorgeReyesSDG <jorge.reyes@sdggroup.com>
2023-11-14 18:24:06 +0100

    Upload userName property and setter

--- code/sap.tnt.sample.ToolHeader/customWidget/widget_aps.js
+++ code/sap.tnt.sample.ToolHeader/customWidget/widget_aps.js
@@ -1,7 +1,7 @@
 (function () {
     let tmpl = document.createElement("template");
     tmpl.innerHTML = 
-    `<button type="button" id="myHeaderBtn">v 35</button>` ;   
+    `<button type="button" id="myHeaderBtn">v 36</button>` ;   
 
     class CustomHeaderAps extends HTMLElement {
        constructor() {
