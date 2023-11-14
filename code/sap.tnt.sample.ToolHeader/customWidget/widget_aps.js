21fb08c34d2e8beeb20d0d5ac293538ff4d3849f
JorgeReyesSDG <jorge.reyes@sdggroup.com>
2023-11-14 17:44:38 +0100

    modify the css

--- code/sap.tnt.sample.ToolHeader/customWidget/widget_aps.js
+++ code/sap.tnt.sample.ToolHeader/customWidget/widget_aps.js
@@ -1,7 +1,7 @@
 (function () {
     let tmpl = document.createElement("template");
     tmpl.innerHTML = 
-    `<button type="button" id="myHeaderBtn">v 31</button>` ;   
+    `<button type="button" id="myHeaderBtn">v 32</button>` ;   
 
     class CustomHeaderAps extends HTMLElement {
        constructor() {
